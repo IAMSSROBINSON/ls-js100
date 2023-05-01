b1c6318cbcc85c89fef1f493e159a6cdb9cf9e9f
SSR <iamssrobinson@gmail.com>
2023-05-01 10:07:20 +0100

    Exercise 2: let and const variable declarations. String literal, template literals, string interpolation, expressions.

--- 01_intro_to_programming/02_variables/06_exercises/02_age.js
+++ 01_intro_to_programming/02_variables/06_exercises/02_age.js
@@ -12,6 +12,7 @@ const TWENTY_INT = 20;
 const THIRTY_INT = 30;
 const FORTY_INT = 40;
 
+console.log(`You are ${age} years old.`);
 console.log(`In ${TEN_INT} years, you will be ${age + TEN_INT} years old.`);
 console.log(`In ${TWENTY_INT} years, you will be ${age + TWENTY_INT} years old.`);
 console.log(`In ${THIRTY_INT} years, you will be ${age + THIRTY_INT} years old.`);
@@ -23,12 +24,12 @@ On line 8 the 'let' keyword is used as a block scoped identifier to declare a va
 
 On lines 10, 11, 12 and 13 the const keyword is used as to declare block scoped variables and give them an initializer value as is required by convention. These values are immutable and cannot change for as long as they are in use and they were given this keyword for that sole purpose. 
 
-On line 15 the log method of the console object is invoked and an interpolated string is passed in as an argument. The string is interpolated because it is seen to use backticks `` with the inclusion of string literals and the ${} template literal symbols to whereby the 
-expression value is inserted into the resulting string printed to the console / terminal window. 
+On line 15 the log method of the console object is invoked and an interpolated string is passed in as an argument. The string is interpolated because it is seen to use string literals inside backticks `` with the inclusion of ${} template literal symbols whereby the 
+expression value is inserted / interpolated into the resulting string printed to the console / terminal window. 
 
 The expression is anything that JavaScript can evaluate to a value that can be captured and used in the code and thus the value of the variables referenced is used to perform addition using the arithmetic + operator to add the values of both operand expressions for a final expression value. This shows that template literals have the ability to use expression values in a resulting string literal and also they operations can be performed within template literals to the same effect.
 
-On lines 16, 17 and 18 these principles are repeated with adjustments to the variables used in the expressions to print differing results:
+On lines 16, 17, 18 and 19 these principles are repeated with adjustments to the variables used in the expressions to print differing results:
 
 In 10 years, you will be 30 years old.
 In 20 years, you will be 40 years old.

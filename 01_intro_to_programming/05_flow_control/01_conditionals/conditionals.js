a6937a87ea2a20e513f90bd8495714376d5175b7
SSR <iamssrobinson@gmail.com>
2023-05-08 13:11:11 +0100

    Minor amendment.

---
+++ 01_intro_to_programming/05_flow_control/01_conditionals/conditionals.js
@@ -0,0 +1,11 @@
+let a = prompt(`Enter a number`);
+
+if (a === '3') {
+  console.log(`A is 3`);
+}
+else if (a === '4') {
+  console.log(`A is 4`);
+}
+else {
+  console.log(`A is neither 3, nor 4`);
+}

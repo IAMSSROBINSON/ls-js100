128f9fb264465291fac32dc45775a01071dbc44d
SSR <iamssrobinson@gmail.com>
2023-05-13 13:07:50 +0100

    Understanding what arrays are.

---
+++ 01_intro_to_programming/07_arrays/01_what_is_an_array/heterogeneousArray.js
@@ -0,0 +1,15 @@
+// store any datatype in an array
+let heterogeneousArray = ["string", 100, true, {color: 'yellow'}, [10, 20]];
+console.log(heterogeneousArray);
+
+// indexed, ordered lists
+console.log(heterogeneousArray[2]);
+
+// the first element is at index 0
+console.log(heterogeneousArray[0]);
+
+// access the last element of the array no matter how many elements
+console.log(heterogeneousArray[heterogeneousArray.length - 1]);
+
+
+
---
+++ 01_intro_to_programming/07_arrays/01_what_is_an_array/notes.md
@@ -0,0 +1,10 @@
+# What Is An Array?
+
+Arrays are indexed, ordered lists of elements of any data type meaning they are heterogeneous and are defined with square brackets. 
+
+<br>
+
+## Array Index
+
+Each element has a unique index number that references its position in the array and the value present.
+

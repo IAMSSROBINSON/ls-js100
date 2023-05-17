f4a7ddee5f807a1f401c5be2f2ac3ab164e8507e
SSR <iamssrobinson@gmail.com>
2023-05-17 18:18:30 +0100

    Amended description of conditional to comparison expression.

--- 02_exercises_javascript_basics/03_loops/07_add_on_and_on_and_on.js
+++ 02_exercises_javascript_basics/03_loops/07_add_on_and_on_and_on.js
@@ -13,8 +13,8 @@ for (let i = 0; i < 1 ; i += 1) {
 
 The code is in an endless loop because on the first line of code a variable is declared and initialized to the value of 1. 
 
-Typically a conditional expression would appear next in the for clause but instead that is skipped and so JavaScript just evaluate it as true, then variable is lastly incremented by +1 on each iteration by the incremental assignment operator. 
+Typically a comparison expression would appear next in the for clause but instead that is skipped and so JavaScript just evaluates it as true. Then the variable is lastly incremented by +1 on each iteration by the incremental assignment operator. 
 
-Because the conditional expression is skipped, JavaScript by default will assume it to be true and the for clause will always increase the value of the variable by +1 on each iteration and then enter the execution body and invoke the console.log method and print to the console. There is therefore nothing to break the cycle of the loop or render the condition as false and thus it will continue forever or until a break statement is encountered or until the program is quit.
+Because the comparison expression is skipped, JavaScript by default will assume it to be true and the 'for' clause will always increase the value of the variable by +1 on each iteration. Then the block body will be entered and invoke the console.log method and print to the console. There is therefore nothing to break the cycle of the loop or render the 'for' clause as false and thus it will continue forever or until a break statement is encountered or until the program is quit.
 
 */
